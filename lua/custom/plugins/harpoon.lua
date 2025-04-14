83f1c3b8ad6a607e564afa1775c1ecc749502efa
Casper Olsen <cabol@energinet.dk>
2025-04-14 15:08:00 +0200

    Harpoon setup

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -6,6 +6,30 @@ return {
     local harpoon = require 'harpoon'
     harpoon:setup()
 
+    -- basic telescope configuration
+    local conf = require('telescope.config').values
+    local function toggle_telescope(harpoon_files)
+      local file_paths = {}
+      for _, item in ipairs(harpoon_files.items) do
+        table.insert(file_paths, item.value)
+      end
+
+      require('telescope.pickers')
+        .new({}, {
+          prompt_title = 'Harpoon',
+          finder = require('telescope.finders').new_table {
+            results = file_paths,
+          },
+          previewer = conf.file_previewer {},
+          sorter = conf.generic_sorter {},
+        })
+        :find()
+    end
+
+    vim.keymap.set('n', '<leader>sh', function()
+      toggle_telescope(harpoon:list())
+    end, { desc = '[S]earch [H]arpoon' })
+
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end, { desc = '[A]dd Harpoon' })
