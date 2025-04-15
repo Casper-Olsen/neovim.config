a7268265532282cbebfa1a8ab700301bf8bcceb1
Casper Olsen <cabol@energinet.dk>
2025-04-15 10:02:38 +0200

    Added markdown support

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -5,6 +5,14 @@ return {
     require('render-markdown').setup {
       enabled = false,
       completions = { lsp = { enabled = true } },
+
+      vim.keymap.set('n', '<leader>me', function()
+        require('render-markdown').enable()
+      end, { desc = '[M]arkdown [E]nable' }),
+
+      vim.keymap.set('n', '<leader>md', function()
+        require('render-markdown').disable()
+      end, { desc = '[M]arkdown [D]isable' }),
     }
   end,
 }
