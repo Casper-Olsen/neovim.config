d253287c33621ba0d36f18a7d0222213a00e1927
Casper Olsen <gemic@live.dk>
2025-04-19 18:16:32 +0200

    markdown config

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,18 +1,32 @@
 return {
-  'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
-  config = function()
-    require('render-markdown').setup {
-      enabled = false,
-      completions = { lsp = { enabled = true } },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
+    config = function()
+      require('render-markdown').setup {
+        enabled = false,
+        completions = { lsp = { enabled = true } },
 
-      vim.keymap.set('n', '<leader>me', function()
-        require('render-markdown').enable()
-      end, { desc = '[M]arkdown [E]nable' }),
+        vim.keymap.set('n', '<leader>me', function()
+          require('render-markdown').enable()
+        end, { desc = '[M]arkdown [E]nable' }),
 
-      vim.keymap.set('n', '<leader>md', function()
-        require('render-markdown').disable()
-      end, { desc = '[M]arkdown [D]isable' }),
-    }
-  end,
+        vim.keymap.set('n', '<leader>md', function()
+          require('render-markdown').disable()
+        end, { desc = '[M]arkdown [D]isable' }),
+      }
+    end,
+  },
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    build = 'cd app && yarn install',
+    init = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+    config = function()
+      vim.keymap.set('n', '<leader>mp', ':MarkdownPreviewToggle<CR>', { desc = '[M]arkdown [P]review' })
+    end,
+  },
 }
