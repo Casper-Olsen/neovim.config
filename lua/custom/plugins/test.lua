d0384cd2cce752f4a8c6b7a66e366282947c6711
Casper Olsen <gemic@live.dk>
2025-04-27 16:53:10 +0200

    use cmd in test commands

--- lua/custom/plugins/test.lua
+++ lua/custom/plugins/test.lua
@@ -4,11 +4,11 @@ return {
     'preservim/vimux',
   },
 
-  vim.keymap.set('n', '<leader>tn', ':TestNearest<CR>', { desc = '[T]est [N]earest' }),
-  vim.keymap.set('n', '<leader>tf', ':TestFile<CR>', { desc = '[T]est [F]ile' }),
-  vim.keymap.set('n', '<leader>ts', ':TestSuite<CR>', { desc = '[T]est [S]uite' }),
-  vim.keymap.set('n', '<leader>tl', ':TestLast<CR>', { desc = '[T]est [L]ast' }),
-  vim.keymap.set('n', '<leader>tv', ':TestVisit<CR>', { desc = '[T]est [V]isit' }),
+  vim.keymap.set('n', '<leader>tn', '<cmd>TestNearest<CR>', { desc = '[T]est [N]earest' }),
+  vim.keymap.set('n', '<leader>tf', '<cmd>TestFile<CR>', { desc = '[T]est [F]ile' }),
+  vim.keymap.set('n', '<leader>ts', '<cmd>TestSuite<CR>', { desc = '[T]est [S]uite' }),
+  vim.keymap.set('n', '<leader>tl', '<cmd>TestLast<CR>', { desc = '[T]est [L]ast' }),
+  vim.keymap.set('n', '<leader>tv', '<cmd>TestVisit<CR>', { desc = '[T]est [V]isit' }),
 
   vim.cmd "let test#csharp#runner = 'dotnettest'",
   vim.cmd "let test#strategy = 'vimux'",
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -81,6 +81,24 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
+    -- CoreClr
+    -- dap.adapters.coreclr = {
+    --   type = 'executable',
+    --   command = '/usr/local/bin/netcoredbg',
+    --   args = { '--interpreter=vscode' },
+    -- }
+    --
+    -- dap.configurations.cs = {
+    --   {
+    --     type = 'coreclr',
+    --     name = 'launch - netcoredbg',
+    --     request = 'launch',
+    --     program = function()
+    --       return vim.fn.input('Path to dll', vim.fn.getcwd() .. '/bin/Debug/', 'file')
+    --     end,
+    --   },
+    -- }
+
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
