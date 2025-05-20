7a4239ef84288e9d1fd58bcf5f903e62af82f556
Casper Olsen <cabol@energinet.dk>
2025-05-20 13:28:31 +0200

    tmux

--- lua/custom/plugins/vimtmuxnavigator.lua
+++ lua/custom/plugins/vimtmuxnavigator.lua
@@ -1,18 +1,23 @@
 return {
-  'christoomey/vim-tmux-navigator',
-  cmd = {
-    'TmuxNavigateLeft',
-    'TmuxNavigateDown',
-    'TmuxNavigateUp',
-    'TmuxNavigateRight',
-    'TmuxNavigatePrevious',
-    'TmuxNavigatorProcessList',
+  {
+    'christoomey/vim-tmux-navigator',
+    cmd = {
+      'TmuxNavigateLeft',
+      'TmuxNavigateDown',
+      'TmuxNavigateUp',
+      'TmuxNavigateRight',
+      'TmuxNavigatePrevious',
+      'TmuxNavigatorProcessList',
+    },
+    keys = {
+      { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<CR>' },
+      { '<c-j>', '<cmd><C-U>TmuxNavigateDown<CR>' },
+      { '<c-k>', '<cmd><C-U>TmuxNavigateUp<CR>' },
+      { '<c-l>', '<cmd><C-U>TmuxNavigateRight<CR>' },
+      { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<CR>' },
+    },
   },
-  keys = {
-    { '<c-h>', '<cmd><C-U>TmuxNavigateLeft<CR>' },
-    { '<c-j>', '<cmd><C-U>TmuxNavigateDown<CR>' },
-    { '<c-k>', '<cmd><C-U>TmuxNavigateUp<CR>' },
-    { '<c-l>', '<cmd><C-U>TmuxNavigateRight<CR>' },
-    { '<c-\\>', '<cmd><C-U>TmuxNavigatePrevious<CR>' },
+  {
+    'preservim/vimux',
   },
 }
