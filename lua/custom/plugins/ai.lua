34663e557c5ef97d91bdfc56b2f10edbd4f64843
Casper Olsen <cabol@energinet.dk>
2025-06-13 15:22:37 +0200

    keymap

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -18,7 +18,7 @@ return {
 
         vim.keymap.set({ 'n', 'v' }, '<leader>cc', '<cmd>CodeCompanionChat Toggle<CR>', { desc = '[C]odeCompanion [C]hat', noremap = true, silent = true }),
         vim.keymap.set('v', '<leader>cy', '<cmd>CodeCompanionChat<CR>', { desc = '[C]odeCompanion Chat [Y]ank (add)', noremap = true, silent = true }),
-        vim.keymap.set({ 'n', 'v' }, '<C-a>', '<cmd>CodeCompanionActions<CR>', { desc = '[C]odeCompanion [A]ctions', noremap = true, silent = true }),
+        vim.keymap.set({ 'n', 'v' }, '<leader>cA', '<cmd>CodeCompanionActions<CR>', { desc = '[C]odeCompanion [A]ctions', noremap = true, silent = true }),
 
         -- Expand 'cc' into 'CodeCompanion' in the command line
         vim.cmd [[cab cc CodeCompanion]],
