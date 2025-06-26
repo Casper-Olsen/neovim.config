8916f8342617e2178ce90ee3a420d5de1daa3e52
Casper Olsen <cabol@energinet.dk>
2025-06-26 09:06:29 +0200

    diff view

--- lua/custom/plugins/diffview.lua
+++ lua/custom/plugins/diffview.lua
@@ -1,7 +1,13 @@
 return {
   'sindrets/diffview.nvim',
 
-  vim.keymap.set('n', '<leader>dw', '<cmd>DiffviewOpen<CR>', { desc = '[D]iff[V]iew' }),
-  vim.keymap.set('n', '<leader>df', '<cmd>DiffviewFileHistory %<CR>', { desc = '[D]iffview [F]ile history' }),
-  vim.keymap.set('n', '<leader>dc', '<cmd>DiffviewClose<CR>', { desc = '[D]iffview [C]lose' }),
+  config = function()
+    require('diffview').setup {
+      use_icons = false,
+
+      vim.keymap.set('n', '<leader>dw', '<cmd>DiffviewOpen<CR>', { desc = '[D]iff[V]iew' }),
+      vim.keymap.set('n', '<leader>df', '<cmd>DiffviewFileHistory %<CR>', { desc = '[D]iffview [F]ile history' }),
+      vim.keymap.set('n', '<leader>dc', '<cmd>DiffviewClose<CR>', { desc = '[D]iffview [C]lose' }),
+    }
+  end,
 }
