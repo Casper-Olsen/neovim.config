412cfbd863ce639f9a793541d5f76303cce7e241
Casper Olsen <cabol@energinet.dk>
2025-07-16 18:57:34 +0200

    Run diag for roslyn on autocmd

--- init.lua
+++ init.lua
@@ -23,5 +23,8 @@ require 'lazy-plugins'
 -- [[ Custom commands ]]
 require 'commands'
 
+-- [[ Autocommands]]
+require 'autocommands'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
