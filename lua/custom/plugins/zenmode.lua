d97e42b3df18dbb60ea09915ed86aef196042d3d
Casper Olsen <cabol@energinet.dk>
2025-07-22 12:38:18 +0200

    Zen mode default width

--- lua/custom/plugins/zenmode.lua
+++ lua/custom/plugins/zenmode.lua
@@ -1,6 +1,12 @@
 return {
   'folke/zen-mode.nvim',
+
   config = function()
+    require('zen-mode').setup {
+      window = {
+        width = 150,
+      },
+    }
     vim.keymap.set('n', '<leader>zm', '<cmd>ZenMode<CR>', { desc = '[Z]en [M]ode' })
   end,
 }
