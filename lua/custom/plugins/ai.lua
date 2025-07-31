6d3cefa8959c8aeb03ab3cd6222a4c1daf3b5c39
Casper Olsen <cabol@energinet.dk>
2025-07-31 08:33:26 +0200

    Switch to copilot chat

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -8,46 +8,22 @@ return {
       require('copilot').setup {
         suggestion = { enabled = false },
         panel = { enabled = false },
+        -- debug = true,
       }
     end,
   },
   {
-    'olimorris/codecompanion.nvim',
-    version = 'v16.3.0',
-    config = function()
-      require('codecompanion').setup {
-
-        vim.keymap.set({ 'n', 'v' }, '<leader>cc', '<cmd>CodeCompanionChat Toggle<CR>', { desc = '[C]odeCompanion [C]hat', noremap = true, silent = true }),
-        vim.keymap.set('v', '<leader>cy', '<cmd>CodeCompanionChat<CR>', { desc = '[C]odeCompanion Chat [Y]ank (add)', noremap = true, silent = true }),
-        vim.keymap.set({ 'n', 'v' }, '<leader>cA', '<cmd>CodeCompanionActions<CR>', { desc = '[C]odeCompanion [A]ctions', noremap = true, silent = true }),
-
-        -- Expand 'cc' into 'CodeCompanion' in the command line
-        vim.cmd [[cab cc CodeCompanion]],
-
-        strategies = {
-          chat = {
-            adapter = {
-              name = 'copilot',
-              model = 'claude-3.7-sonnet',
-            },
-          },
-          inline = {
-            adapter = {
-              name = 'copilot',
-              model = 'claude-3.7-sonnet',
-            },
-          },
-        },
-        display = {
-          action_palette = {
-            show_default_prompt_library = true,
-          },
-        },
-      }
-    end,
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
+    {
+      'CopilotC-Nvim/CopilotChat.nvim',
+      dependencies = {
+        { 'nvim-lua/plenary.nvim', branch = 'master' },
+      },
+      build = 'make tiktoken',
+      opts = {
+        model = 'gpt-4.1',
+        -- model = 'claude-sonnet-3.7',
+      },
+      vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChatToggle<CR>', { desc = 'Copilot Chat' }),
     },
   },
 }
