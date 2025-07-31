4033b14b5bfea831c9f097ea84b55c9ccb754a1b
Casper Olsen <cabol@energinet.dk>
2025-07-31 12:17:41 +0200

    Which key desc

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -59,7 +59,7 @@ return {
 
       -- Document existing key chains
       spec = {
-        { '<leader>c', group = '[C]ode & [C]odeCompanion', mode = { 'n', 'x' } },
+        { '<leader>c', group = '[C]ode & [C]odeCompanion & [C]opilot', mode = { 'n', 'x' } },
         { '<leader>d', group = '[D]ocument & [D]ebug & [D]iagnostic & [D]iff' },
         { '<leader>r', group = '[R]ename & [R]oslyn' },
         { '<leader>s', group = '[S]earch' },
