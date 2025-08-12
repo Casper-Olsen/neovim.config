a6be83e686b61fc47ff7b8e9354ea1595e3be7df
Casper Olsen <cabol@energinet.dk>
2025-08-12 09:57:50 +0200

    feat: use vim lsp enable

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -46,11 +46,6 @@ return {
       vim.api.nvim_create_autocmd('LspAttach', {
         group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
         callback = function(event)
-          -- NOTE: Remember that Lua is a real programming language, and as such it is possible
-          -- to define small helper and utility functions so you don't have to repeat yourself.
-          --
-          -- In this case, we create a function that lets us more easily define mappings specific
-          -- for LSP related items. It sets the mode, buffer and description for us each time.
           local map = function(keys, func, desc, mode)
             mode = mode or 'n'
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
@@ -182,74 +177,53 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-      local servers = {
-        clangd = {
-          cmd = {
-            'clangd',
-            '--offset-encoding=utf-16',
-            '--background-index',
-            '--clang-tidy',
-            '--completion-style=detailed',
-            '--suggest-missing-includes',
-            '--cross-file-rename',
-          },
-          init_options = {
-            fallbackFlags = { '-std=c++23' },
+      vim.lsp.config.clangd = {
+        cmd = {
+          'clangd',
+          '--offset-encoding=utf-16',
+          '--background-index',
+          '--clang-tidy',
+          '--completion-style=detailed',
+          '--suggest-missing-includes',
+          '--cross-file-rename',
+        },
+        init_options = {
+          fallbackFlags = { '-std=c++23' },
+        },
+        capabilities = capabilities,
+        root_markers = { 'compile_commands.json', 'compile_flags.txt' },
+        filetypes = { 'c', 'cpp' },
+      }
+      vim.lsp.config.ts_ls = {
+        settings = {
+          tsserver_file_preferences = {
+            includeInlayParameterNameHints = 'literals',
+            includeInlayHintsForImplicitVariableTypes = false,
           },
         },
-        ts_ls = {
-          settings = {
-            tsserver_file_preferences = {
-              includeInlayParameterNameHints = 'literals',
-              includeInlayHintsForImplicitVariableTypes = false,
-            },
+        capabilities = capabilities,
+      }
+
+      vim.lsp.config.lua_ls = {
+        settings = {
+          Lua = {
+            completion = { callSnippet = 'Replace' },
           },
         },
-        lua_ls = {
-          settings = {
-            Lua = {
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
+        capabilities = capabilities,
+      }
+
+      vim.lsp.enable { 'clangd', 'ts_ls', 'lua_ls' }
+
+      require('mason-tool-installer').setup {
+        ensure_installed = {
+          'stylua', -- for Lua formatting
         },
       }
 
-      -- Ensure the servers and tools above are installed
-      --
-      -- To check the current status of installed tools and/or manually install
-      -- other tools, you can run
-      --    :Mason
-      --
-      -- You can press `g?` for help in this menu.
-      --
-      -- `mason` had to be setup earlier: to configure its options see the
-      -- `dependencies` table for `nvim-lspconfig` above.
-      --
-      -- You can add other tools here that you want Mason to install
-      -- for you, so that they are available from within Neovim.
-      local ensure_installed = vim.tbl_keys(servers or {})
-      vim.list_extend(ensure_installed, {
-        'stylua', -- Used to format Lua code
-      })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
       require('mason-lspconfig').setup {
-        ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+        ensure_installed = {},
         automatic_installation = false,
-        handlers = {
-          function(server_name)
-            local server = servers[server_name] or {}
-            -- This handles overriding only values explicitly passed
-            -- by the server configuration above. Useful when disabling
-            -- certain features of an LSP (for example, turning off formatting for ts_ls)
-            server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
-            require('lspconfig')[server_name].setup(server)
-          end,
-        },
       }
     end,
   },
