afc34e57dd05849411d73b5dfc4adf1de388f922
Casper Olsen <cabol@energinet.dk>
2025-09-08 13:06:27 +0200

    feat: test command for running test a sudo

--- lua/custom/plugins/test.lua
+++ lua/custom/plugins/test.lua
@@ -1,32 +1,29 @@
 return {
   'vim-test/vim-test',
-  dependencies = {
-    'preservim/vimux',
-  },
+  dependencies = { 'preservim/vimux' },
 
-  vim.keymap.set('n', '<leader>tn', '<cmd>TestNearest<CR>', { desc = '[T]est [N]earest' }),
-  vim.keymap.set('n', '<leader>tf', '<cmd>TestFile<CR>', { desc = '[T]est [F]ile' }),
-  vim.keymap.set('n', '<leader>ts', '<cmd>TestSuite<CR>', { desc = '[T]est [S]uite' }),
-  vim.keymap.set('n', '<leader>tl', '<cmd>TestLast<CR>', { desc = '[T]est [L]ast' }),
-  vim.keymap.set('n', '<leader>tv', '<cmd>TestVisit<CR>', { desc = '[T]est [V]isit' }),
+  config = function()
+    vim.keymap.set('n', '<leader>te', function()
+      vim.cmd 'TestNearest -strategy=sudo_vimux'
+    end, { desc = '[T]est [N]earest [E]levated' })
 
-  vim.cmd "let test#csharp#runner = 'dotnettest'",
-  vim.cmd "let test#strategy = 'vimux'",
+    vim.keymap.set('n', '<leader>tn', '<cmd>TestNearest<CR>', { desc = '[T]est [N]earest' })
+    vim.keymap.set('n', '<leader>tf', '<cmd>TestFile<CR>', { desc = '[T]est [F]ile' })
+    vim.keymap.set('n', '<leader>ts', '<cmd>TestSuite<CR>', { desc = '[T]est [S]uite' })
+    vim.keymap.set('n', '<leader>tl', '<cmd>TestLast<CR>', { desc = '[T]est [L]ast' })
+    vim.keymap.set('n', '<leader>tv', '<cmd>TestVisit<CR>', { desc = '[T]est [V]isit' })
 
-  -- Not tested if it works yet
-  -- vim.g['test#custom_strategies']
-  --   == {
-  --     dap_debug = function(cmd)
-  --       local dap = require 'dap'
-  --       dap.run {
-  --         type = 'cppdbg',
-  --         request = 'launch',
-  --         name = 'Debug Test',
-  --         program = cmd[2],
-  --         args = { unpack(cmd, 3) },
-  --         cwd = vim.fn.getcwd(),
-  --         stopOnEntry = false,
-  --       }
-  --     end,
-  --   },
+    vim.g['test#csharp#runner'] = 'dotnettest'
+    vim.g['test#strategy'] = 'vimux'
+
+    vim.g['test#custom_strategies'] = {
+      sudo_vimux = function(cmd)
+        if type(cmd) == 'string' then
+          cmd = { cmd }
+        end
+
+        vim.fn['VimuxRunCommand']('sudo ' .. table.concat(cmd, ' '))
+      end,
+    }
+  end,
 }
