1617648d6698511a3fbd2baa9e1bea5ee2bcc241
Casper Olsen <cabol@energinet.dk>
2025-09-10 08:57:12 +0200

    feat: Removed hover keymap and added hover signature

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -101,9 +101,11 @@ return {
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
-          map('<leader>hi', function()
-            vim.lsp.buf.hover()
-          end, '[H]over [I]nfo')
+          -- Hover information for symbol (vim.lsp.buf.hover()) -> K to show, KK to show and focus
+
+          map('<leader>hs', function()
+            vim.lsp.buf.signature_help()
+          end, '[H]over [S]ignature')
 
           ---@param client vim.lsp.Client
           ---@param method vim.lsp.protocol.Method
