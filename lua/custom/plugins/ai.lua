033882c7ed1993f59efbb41898e79289c8d19e6c
Casper Olsen <cabol@energinet.dk>
2025-09-11 09:50:21 +0200

    feat: Moved to copilot chat

--- lua/custom/plugins/ai.lua
+++ lua/custom/plugins/ai.lua
@@ -20,43 +20,15 @@ return {
     end,
   },
   {
-    'olimorris/codecompanion.nvim',
-    config = function()
-      require('codecompanion').setup {
-
-        vim.keymap.set({ 'n', 'v' }, '<leader>cc', '<cmd>CodeCompanionChat Toggle<CR>', { desc = '[C]odeCompanion [C]hat', noremap = true, silent = true }),
-        vim.keymap.set('v', '<leader>cy', '<cmd>CodeCompanionChat<CR>', { desc = '[C]odeCompanion Chat [Y]ank (add)', noremap = true, silent = true }),
-        vim.keymap.set({ 'n', 'v' }, '<leader>cA', '<cmd>CodeCompanionActions<CR>', { desc = '[C]odeCompanion [A]ctions', noremap = true, silent = true }),
-
-        -- Expand 'cc' into 'CodeCompanion' in the command line
-        vim.cmd [[cab cc CodeCompanion]],
-
-        strategies = {
-          chat = {
-            adapter = {
-              name = 'copilot',
-              -- model = 'gpt-4.1',
-              model = 'claude-3.7-sonnet',
-            },
-          },
-          inline = {
-            adapter = {
-              name = 'copilot',
-              -- model = 'gpt-4.1',
-              model = 'claude-3.7-sonnet',
-            },
-          },
-        },
-        display = {
-          action_palette = {
-            show_default_prompt_library = true,
-          },
-        },
-      }
-    end,
+    'CopilotC-Nvim/CopilotChat.nvim',
     dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
+      { 'nvim-lua/plenary.nvim', branch = 'master' },
     },
+    build = 'make tiktoken',
+    opts = {
+      -- model = 'gpt-4.1',
+      model = 'claude-3.7-sonnet',
+    },
+    vim.keymap.set('n', '<leader>cc', '<cmd>CopilotChatToggle<CR>', { desc = 'Copilot Chat' }),
   },
 }
