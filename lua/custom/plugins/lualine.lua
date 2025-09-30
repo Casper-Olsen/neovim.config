6579984c0e9dc927637b1fa9a1c1fded7ec1e554
Casper Olsen <cabol@energinet.dk>
2025-09-30 15:24:01 +0200

    feat: New mini urls and updated plugins and removed lsp_status

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -9,7 +9,7 @@ return {
 
       sections = {
         lualine_a = { 'mode' },
-        lualine_b = { 'branch', 'diff', 'diagnostics', 'lsp_status' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
         lualine_c = { { 'filename', path = 1 } },
         lualine_x = { 'encoding', 'fileformat', 'filetype' },
         lualine_y = {},
