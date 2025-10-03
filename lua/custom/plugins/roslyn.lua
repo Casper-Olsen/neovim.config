21c8e5280cb39fef6f45768738153eba8cf39454
Casper Olsen <cabol@energinet.dk>
2025-10-03 13:27:26 +0200

    feat: Roslyn settings and target keymap

--- lua/custom/plugins/roslyn.lua
+++ lua/custom/plugins/roslyn.lua
@@ -4,8 +4,17 @@ return {
   config = function()
     require('roslyn').setup {
       filewatching = 'roslyn',
+
+      -- Use `:Roslyn target` to change the target .sln
+      lock_target = true,
+
+      ignore_target = function(target)
+        return string.match(target, '%.CI%.sln$') ~= nil
+      end,
     }
 
     vim.keymap.set('n', '<leader>rr', '<cmd>Roslyn restart<CR>', { desc = '[R]oslyn [R]estart' })
+
+    vim.keymap.set('n', '<leader>rt', '<cmd>Roslyn target<CR>', { desc = '[R]oslyn [T]arget' })
   end,
 }
