d5066e128c98b510742c7e01db7c6942029b287f
Casper Olsen <cabol@energinet.dk>
2025-10-13 12:07:13 +0200

    feat: Disabled autopairs generic support in C#

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -19,28 +19,31 @@ return {
         csharp = { 'string' },
       },
     }
-    cmp.event:on('confirm_done', function(event)
-      local entry = event.entry:get_completion_item()
-      local bufnr = vim.api.nvim_get_current_buf()
+    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
 
-      if vim.bo[bufnr].filetype == 'cs' and entry.label and entry.label:match '<.*>' then
-        -- Generic method: replace () with <>
-        vim.schedule(function()
-          local col = vim.fn.col '.'
-          local line = vim.fn.getline '.'
-
-          -- remove () if autopairs inserted it
-          if line:sub(col, col) == ')' then
-            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<BS><BS>', true, false, true), 'n', true)
-          end
-
-          -- insert <> and place cursor inside
-          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<><Left>', true, false, true), 'i', true)
-        end)
-      else
-        -- Normal functions → fallback to autopairs
-        cmp_autopairs.on_confirm_done()(event)
-      end
-    end)
+    -- Inserts <> for generics in C#
+    -- cmp.event:on('confirm_done', function(event)
+    --   local entry = event.entry:get_completion_item()
+    --   local bufnr = vim.api.nvim_get_current_buf()
+    --
+    --   if vim.bo[bufnr].filetype == 'cs' and entry.label and entry.label:match '<.*>' then
+    --     -- Generic method: replace () with <>
+    --     vim.schedule(function()
+    --       local col = vim.fn.col '.'
+    --       local line = vim.fn.getline '.'
+    --
+    --       -- remove () if autopairs inserted it
+    --       if line:sub(col, col) == ')' then
+    --         vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<BS><BS>', true, false, true), 'n', true)
+    --       end
+    --
+    --       -- insert <> and place cursor inside
+    --       vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<><Left>', true, false, true), 'i', true)
+    --     end)
+    --   else
+    --     -- Normal functions → fallback to autopairs
+    --     cmp_autopairs.on_confirm_done()(event)
+    --   end
+    -- end)
   end,
 }
