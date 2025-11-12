d96564c80c934b0e7d210a42c7e7059fe4e0470e
Casper Olsen <cabol@energinet.dk>
2025-11-12 08:14:20 +0100

    feat: pin version

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -3,6 +3,7 @@ return {
   -- cp /mnt/c/WINDOWS/system32/cmd.exe $HOME/.local/bin/
 
   'iamcco/markdown-preview.nvim',
+  commit = 'a923f5fc5ba36a3b17e289dc35dc17f66d0548ee',
   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
   build = 'cd app && yarn install',
   init = function()
