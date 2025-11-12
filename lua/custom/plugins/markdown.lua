4cfd4eff73cf9e3fa9d1de81580c135221c15710
Casper Olsen <cabol@energinet.dk>
2025-11-12 08:12:13 +0100

    feat: Remove markdown enable/disable

--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,36 +1,15 @@
 return {
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    version = 'v8.10.0',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
-    config = function()
-      require('render-markdown').setup {
-        enabled = false,
-        completions = { lsp = { enabled = true } },
+  -- Fix to the following error: "Can not open browse by using cmd.exe command"
+  -- cp /mnt/c/WINDOWS/system32/cmd.exe $HOME/.local/bin/
 
-        vim.keymap.set('n', '<leader>me', function()
-          require('render-markdown').enable()
-        end, { desc = '[M]arkdown [E]nable' }),
-
-        vim.keymap.set('n', '<leader>md', function()
-          require('render-markdown').disable()
-        end, { desc = '[M]arkdown [D]isable' }),
-      }
-    end,
-  },
-  {
-    -- Fix to the following error: "Can not open browse by using cmd.exe command"
-    -- cp /mnt/c/WINDOWS/system32/cmd.exe $HOME/.local/bin/
-
-    'iamcco/markdown-preview.nvim',
-    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-    build = 'cd app && yarn install',
-    init = function()
-      vim.g.mkdp_filetypes = { 'markdown' }
-    end,
-    ft = { 'markdown', 'codecompanion' },
-    config = function()
-      vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<CR>', { desc = '[M]arkdown [P]review' })
-    end,
-  },
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && yarn install',
+  init = function()
+    vim.g.mkdp_filetypes = { 'markdown' }
+  end,
+  ft = { 'markdown', 'codecompanion' },
+  config = function()
+    vim.keymap.set('n', '<leader>mp', '<cmd>MarkdownPreview<CR>', { desc = '[M]arkdown [P]review' })
+  end,
 }
