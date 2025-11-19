6be70ab61b96fa5e3c755af93ff82d5141eaa3a6
Casper Olsen <cabol@energinet.dk>
2025-11-19 11:53:46 +0100

    fix: correct comment for vim fugitive commands

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -7,8 +7,8 @@ return {
       vim.keymap.set('n', '<leader>gx', '<cmd>Git push<CR>', { desc = '[G]it [P]ush' })
 
       vim.keymap.set('n', '<leader>gm', '<cmd>Gvdiffsplit!<CR>', { desc = '[G]it 3-way [M]erge' })
-      vim.keymap.set('n', '<leader>gh', '<cmd>diffget //2<CR>', { desc = '[G]it take OURS (//2)' })
-      vim.keymap.set('n', '<leader>gl', '<cmd>diffget //3<CR>', { desc = '[G]it take THEIRS (//3)' })
+      vim.keymap.set('n', '<leader>gh', '<cmd>diffget //2<CR>', { desc = '[G]it take CURRENT branch (//2)' })
+      vim.keymap.set('n', '<leader>gl', '<cmd>diffget //3<CR>', { desc = '[G]it take INCOMING branch (//3)' })
       vim.keymap.set('n', '<leader>gn', '/\\v^[<|=]{7}( .*)?$<CR>', { desc = '[G]it NEXT conflict' })
       vim.keymap.set('n', '<leader>gp', '?\\v^[<|=]{7}( .*)?$<CR>', { desc = '[G]it PREV conflict' })
     end,
