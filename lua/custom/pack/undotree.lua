c1d34b99f0acca1acd34cc0b3cde0377edd03893
Casper Olsen <gemic@live.dk>
2026-06-19 21:48:33 +0200

    refactor: Undotree loads first time it's used

--- lua/custom/pack/undotree.lua
+++ lua/custom/pack/undotree.lua
@@ -1,5 +1,4 @@
-vim.cmd.packadd 'nvim.undotree'
-
 vim.keymap.set('n', '<leader>ut', function()
-  vim.cmd.Undotree()
+  vim.cmd.packadd 'nvim.undotree'
+  require('undotree').open()
 end, { desc = '[U]ndotree [T]oggle' })
