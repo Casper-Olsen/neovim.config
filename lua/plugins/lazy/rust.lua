24d66ed2fa3f2f71b97e334e81cff91ec048ee4d
Casper Olsen <gemic@live.dk>
2026-07-05 15:44:55 +0200

    feat: Keymaps for running rust tests

--- lua/plugins/lazy/rust.lua
+++ lua/plugins/lazy/rust.lua
@@ -5,16 +5,17 @@ return {
 
   config = function()
     vim.g.rustaceanvim = {
+      tools = {
+        test_executor = 'background',
+      },
       server = {
         on_attach = function(_, bufnr)
+          -- Code action menu (quick fixes, refactors, etc.)
           vim.keymap.set('n', '<leader>ca', function()
             vim.cmd.RustLsp 'codeAction'
           end, { buffer = bufnr, desc = '[C]ode [A]ction' })
 
-          vim.keymap.set('n', '<leader>dP', function()
-            vim.cmd.RustLsp { 'debuggables', bang = true }
-          end, { buffer = bufnr, desc = '[D]ebug [P]revious Pick' })
-
+          -- Pick a debuggable target with optional CLI args
           vim.keymap.set('n', '<leader>dp', function()
             vim.ui.input({ prompt = 'Debug args: ' }, function(input)
               -- Esc / cancel → do nothing
@@ -34,6 +35,21 @@ return {
               vim.cmd.RustLsp(vim.list_extend({ 'debuggables' }, args))
             end)
           end, { buffer = bufnr, desc = '[D]ebug [P]ick (with args)' })
+
+          -- Debug the previously selected debuggable target
+          vim.keymap.set('n', '<leader>dP', function()
+            vim.cmd.RustLsp { 'debuggables', bang = true }
+          end, { buffer = bufnr, desc = '[D]ebug [P]revious Pick' })
+
+          -- Run testables in current context (file/module)
+          vim.keymap.set('n', '<leader>tn', function()
+            vim.cmd.RustLsp 'testables'
+          end, { buffer = bufnr, desc = '[T]est [N]earest' })
+
+          -- Re-run the last executed testables
+          vim.keymap.set('n', '<leader>tl', function()
+            vim.cmd.RustLsp { 'testables', bang = true }
+          end, { buffer = bufnr, desc = '[T]est [L]ast' })
         end,
       },
     }
