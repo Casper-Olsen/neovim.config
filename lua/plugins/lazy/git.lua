9f0e15c8d6adcf55adaffb0af4f91d49229ffbab
Casper Olsen <gemic@live.dk>
2026-07-08 17:25:38 +0200

    feat: Fugitive mappings for hunk and entire conflict

--- lua/plugins/lazy/git.lua
+++ lua/plugins/lazy/git.lua
@@ -7,10 +7,12 @@ return {
       vim.keymap.set('n', '<leader>gx', '<cmd>Git push<CR>', { desc = '[G]it [P]ush' })
 
       vim.keymap.set('n', '<leader>gm', '<cmd>Gvdiffsplit!<CR>', { desc = '[G]it 3-way [M]erge' })
-      vim.keymap.set('n', '<leader>gh', 'd2o', { remap = true, desc = '[G]it take CURRENT branch (ours)' })
-      vim.keymap.set('n', '<leader>gl', 'd3o', { remap = true, desc = '[G]it take INCOMING branch (theirs)' })
-      -- vim.keymap.set('n', '<leader>gh', '<cmd>diffget //2<CR>', { desc = '[G]it take CURRENT branch (//2)' })
-      -- vim.keymap.set('n', '<leader>gl', '<cmd>diffget //3<CR>', { desc = '[G]it take INCOMING branch (//3)' })
+
+      vim.keymap.set('n', '<leader>gh', 'd2o', { remap = true, desc = '[G]it take CURRENT hunk (ours)' })
+      vim.keymap.set('n', '<leader>gl', 'd3o', { remap = true, desc = '[G]it take INCOMING hunk (theirs)' })
+      vim.keymap.set('n', '<leader>gH', '<cmd>Gread :2<CR>', { desc = '[G]it take entire CURRENT conflict (ours)' })
+      vim.keymap.set('n', '<leader>gL', '<cmd>Gread :3<CR>', { desc = '[G]it take entire INCOMING conflict (theirs)' })
+
       vim.keymap.set('n', '<leader>gn', '/\\v^[<|=]{7}( .*)?$<CR>', { desc = '[G]it NEXT conflict' })
       vim.keymap.set('n', '<leader>gp', '?\\v^[<|=]{7}( .*)?$<CR>', { desc = '[G]it PREV conflict' })
     end,
