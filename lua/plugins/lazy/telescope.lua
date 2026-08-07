fe3094188e102d3c91ea4c17ee3c4cd2125399f7
Casper Olsen <gemic@live.dk>
2026-08-07 19:46:15 +0200

    feat: Add keymap to search functions

--- lua/plugins/lazy/telescope.lua
+++ lua/plugins/lazy/telescope.lua
@@ -79,6 +79,15 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<leader>sF', function()
+        builtin.lsp_document_symbols {
+          symbols = {
+            'Function',
+            'Method',
+            'Constructor',
+          },
+        }
+      end, { desc = '[S]earch [F]unctions' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
