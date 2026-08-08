d51cdf7af1852e5e02f4deca9543b80cb6cbbd30
Casper Olsen <gemic@live.dk>
2026-08-08 09:06:31 +0200

    refactor: Find functions as leader ff

--- lua/plugins/lazy/telescope.lua
+++ lua/plugins/lazy/telescope.lua
@@ -79,7 +79,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>sF', function()
+      vim.keymap.set('n', '<leader>ff', function()
         builtin.lsp_document_symbols {
           symbols = {
             'Function',
@@ -87,7 +87,7 @@ return {
             'Constructor',
           },
         }
-      end, { desc = '[S]earch [F]unctions' })
+      end, { desc = '[F]ile [F]unctions' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
